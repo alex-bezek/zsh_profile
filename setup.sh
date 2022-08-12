a0439db65a6c2ca708f3684927edd66381ab0198
Alex Bezek <alex@ngrok.com>
2022-08-12 14:17:38 -0700

    new changes

--- setup.sh
+++ setup.sh
@@ -22,28 +22,3 @@ mkdir "$HAMMERSPOON_HOME"
 mkdir "$HAMMERSPOON_HOME/Spoons"
 curl -SL https://github.com/Hammerspoon/Spoons/raw/master/Spoons/SpoonInstall.spoon.zip | tar -xf - -C ~/.hammerspoon/Spoons
 cp -R ./hammerspon ~/.hammerpoon
-
-echo 'Setup ssh config'
-touch ~/.ssh/config
-cat <<EOT >> ~
-Host ssh.spherestage.com
-  LocalForward 2001 127.0.0.1:8080
-Host ssh.cernuksphere.net
-  LocalForward 2002 127.0.0.1:8080
-Host ssh.cernersphere.com
-  LocalForward 2003 127.0.0.1:8080
-Host ssh.us-west-2.devhealtheintent.com
-  LocalForward 2004 127.0.0.1:8080
-Host ssh.us-west-2.staginghealtheintent.com
-  LocalForward 2005 127.0.0.1:8080
-Host ssh.us-west-2.healtheintent.com
-  LocalForward 2006 127.0.0.1:8080
-Host ssh.ca.cernerasp.cernerops.com
-  LocalForward 2007 127.0.0.1:8080
-Host ssh.au.cernerasp.cernerops.com
-  LocalForward 2008 127.0.0.1:8080
-EOT
-
-echo 'Setup pacfile'
-curl -o ~/pacfile https://raw.github.cerner.com/ETS/workstation-config/master/ets.pac
-sudo networksetup -setautoproxyurl Wi-Fi http://localhost:5000/pacfile
